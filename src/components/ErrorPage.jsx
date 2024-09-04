ddbdbeed11234ce9d279d7c1136dfef528f8ccfc
Ananthu AP <ananthuap48@gmail.com>
2024-09-04 19:24:42 +0530

    error boundary added

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorPage = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <img className='w-full' src="https://cdn.dribbble.com/users/1078347/screenshots/2799566/oops.png" alt="" />
+      <img className='w-50' src="https://cdn.dribbble.com/users/1078347/screenshots/2799566/oops.png" alt="" />
     </div>
   )
 }
